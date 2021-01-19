5217ea40ae2b3fcb8d106e7db6b0aecab1abaa80
John Furlong <johnfurlong24@gmail.com>
2021-01-19 16:47:15 -0700

    Back end set up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+var bodyParser = require('body-parser');
+var db = require("./db");
+
+// Express
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json()); //req.body
+
+// Body-parser
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+
+app.get('/getData', function(req, res){
+    db.tx(t => {
+        return t.any('SELECT * FROM testTable;');
+    })
+    .then((d) => {
+        console.log("Successfully returned data!");
+        console.log(d);
+        res.json(d.rows);
+    })
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log('Server started on port ' + PORT));
