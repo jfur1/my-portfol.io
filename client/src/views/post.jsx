33e4b485e31394c771ad3ae2ca6477fee4c97ff6
unknown <tristan.hanna@colorado.edu>
2021-01-29 17:10:33 -0700

    navigation and header functionality

--- client/src/views/post.jsx
+++ client/src/views/post.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Post = (post) => {
     //console.log("Single Post: ", post["data"]);
     const data = post["data"];
-
+    
     const date = data["date_created"];
     //console.log("DATE TYPE: ", typeof date);
     //console.log("DATE: ", date);
@@ -13,7 +13,7 @@ export const Post = (post) => {
     var months = {"01" : "Jan", "02" : "Feb", "03" : "Mar", "04" : "Apr", "05" : "May", "06" : "Jun", "07" : "Jul", "08" : "Aug", "09" : "Sep", "10" : "Oct", "11": "Nov", "12" : "Dec"};
     const month_name = months[month];
     const day = date.slice(8, 10);
-    const date_tag = `Published: ${month_name} / ${day} / ${year}`; 
+    const date_tag = `Published: ${month_name} / ${day} / ${year}`;
     
     return(
         <div className="post-container">
