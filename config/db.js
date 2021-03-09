a5552b4aedd7f916002a775b266c5e7d04cd51ed
John Furlong <johnfurlong24@gmail.com>
2021-03-09 09:12:32 -0700

    fixed conditional db configs

--- config/db.js
+++ config/db.js
@@ -2,19 +2,20 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 // Connect to Database
-// const devConfig = {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD
-// }
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const devConfig = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
+}
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = {
     connectionString: process.env.DATABASE_URL // Heroku Addon
 }
 
-let db = pgp(proConfig);
+console.log("Node env:", process.env.NODE_ENV);
+let db = pgp(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 module.exports = db;
