6f1716bb87e71d3d544908b815ce0e75899451e5
John Furlong <johnfurlong24@gmail.com>
2021-03-09 09:30:44 -0700

    Added inline bootstrap stylesheet to index.html

--- config/db.js
+++ config/db.js
@@ -9,7 +9,6 @@ const devConfig = {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD
 }
-// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = {
     connectionString: process.env.DATABASE_URL // Heroku Addon
