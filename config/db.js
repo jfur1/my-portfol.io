cecd7510746fbcdb85e186668b6a912b654f0088
John Furlong <johnfurlong24@gmail.com>
2021-03-09 08:52:07 -0700

    Reconfig db  code for production env

--- config/db.js
+++ config/db.js
@@ -2,19 +2,20 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 // Connect to Database
-// const devConfig = {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD
-// }
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
-const proConfig = {
-    connectionString: process.env.DATABASE_URL // Heroku Addon
+const devConfig = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
 }
 
-let db = pgp(proConfig);
+const proConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+// const proConfig = {
+//     connectionString: process.env.DATABASE_URL // Heroku Addon
+// }
+
+let db = pgp(process.env.NODE_ENV === 'production' ? devConfig : proConfig);
 
 module.exports = db;
